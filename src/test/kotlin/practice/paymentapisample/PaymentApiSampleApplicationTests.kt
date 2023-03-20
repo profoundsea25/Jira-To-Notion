94f51cd9fb4d0e90df0d440600d9354904a57192
profoundsea25 <profoundsea25@gmail.com>
2023-03-20 21:00:25 +0900

    [#1] 프로젝트 생성, README.md 작성

---
+++ src/test/kotlin/practice/paymentapisample/PaymentApiSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package practice.paymentapisample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaymentApiSampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
